cacefe0d1698d8d0b4e35265485c8cd14df5a51b
Garrett Stevens <stevens.garrett.j@gmail.com>
2022-12-19 22:59:49 +0000

    Make JWT guard global, mark exceptions with public

--- packages/apollo-collaboration-server/src/changes/changes.service.ts
+++ packages/apollo-collaboration-server/src/changes/changes.service.ts
@@ -61,10 +61,7 @@ export class ChangesService {
 
   async create(change: BaseChange, user: string, userToken: string) {
     this.logger.debug(`Requested change: ${JSON.stringify(change)}`)
-    const validationResult = await validationRegistry.backendPreValidate(
-      change,
-      { userModel: this.userModel },
-    )
+    const validationResult = await validationRegistry.backendPreValidate(change)
     if (!validationResult.ok) {
       const errorMessage = validationResult.resultsMessages
       throw new UnprocessableEntityException(
